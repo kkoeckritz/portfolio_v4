862c7fc8b61c7fedb8c98861299029fe44b9d482
Kris Koeckritz <kjk464@humboldt.edu>
2018-11-29 02:10:42 -0800

    fix issues with pushpin, navbar; add contact information

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,15 +1,26 @@
 $(document).ready(function(){
-	initPushpin();
+  initMaterialize();
+  initCustom();
 });
 
-function initPushpin() {
-	
+function initMaterialize() {
+  // sidenav
+  $(document).ready(function(){
+    $('.sidenav').sidenav();
+  });
+  
+  // pushpin
   $('#content').pushpin({
-		top: $("#content").offset().top,
+    top: $("#content").offset().top,
+    offset: 0
 	});
-	
+  
+  // scrollspy
   $(".scrollspy").scrollSpy({
     scrollOffset: 0
   });
-	
+}
+
+function initCustom() {
+  console.log("firing custom funcs");
 }
